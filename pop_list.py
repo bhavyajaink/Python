438bdc1f28a8fc2db1ea7409f2f8ef734ba0d82b
bhavyajaink <85897032+bhavyajaink@users.noreply.github.com>
2021-06-20 15:31:31 +0530

    Add files via upload
    The method pop of list use in these file.

---
+++ pop_list.py
@@ -0,0 +1,6 @@
+#to take index values from user and remove that element from the list
+fr=['bhvaya','komal','khushi','akshuni','divya']
+print('before the pop operation of fr value are:=',fr)
+i=int(input('enter the index value of that place that you want to delete'))
+fr.pop(i)
+print('after the pop opertion of fr value are:=',fr)
