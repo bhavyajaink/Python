4810eeed389c4a7a72c15195dd96fbd7c5210d0b
bhavyajaink <85897032+bhavyajaink@users.noreply.github.com>
2021-06-20 15:33:05 +0530

    Add files via upload
    The method sort of list use in these file.

---
+++ sort_list().py
@@ -0,0 +1,5 @@
+#to sort the element of the list
+fr=['bhvaya','komal','khushi','akshuni','divya']
+print('before the sort operation of fr value are:=',fr)
+fr.sort()
+print('after the sort opertion of fr value are:=',fr)
