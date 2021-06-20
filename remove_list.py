b8c892a304f07acbe79431a78b8141e296618bdb
bhavyajaink <85897032+bhavyajaink@users.noreply.github.com>
2021-06-20 15:32:03 +0530

    Add files via upload
    The method remove of list use in these file.

---
+++ remove_list.py
@@ -0,0 +1,6 @@
+#to take element values from user and remove that element from the list
+fr=['bhvaya','komal','khushi','akshuni','divya']
+print('before the remove operation of fr value are:=',fr)
+i=input('enter the index value of that place that you want to delete')
+fr.remove(i)
+print('after the remove opertion of fr value are:=',fr)
