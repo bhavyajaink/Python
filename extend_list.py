06c545ec373447922ca404da69587ce9f034210a
bhavyajaink <85897032+bhavyajaink@users.noreply.github.com>
2021-06-20 15:29:52 +0530

    Add files via upload
    The method extend of list use in these file.

---
+++ extend_list.py
@@ -0,0 +1,7 @@
+#to write a program to add the elements of one list to another list
+# we use the extend function
+a=['bhvaya','komal','khushi','akshuni','divya']
+b=['depanshi','dikshika','saloni','purvi']
+print('Before the extend of a:=',a)
+a.extend(b)
+print('After the extend of a:=',a)
