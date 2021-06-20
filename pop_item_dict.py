f744648a44b67f18bc29d6dc5e46584a633f7fee
bhavyajaink <85897032+bhavyajaink@users.noreply.github.com>
2021-06-20 15:37:02 +0530

    Add files via upload

---
+++ pop_item_dict.py
@@ -0,0 +1,7 @@
+#use the function of popitem
+dict={"name":("xyz",'bhvy','uh'),
+      "place":("abc",'bhvay','huj'),
+      "age":(12,45,13),
+      "phone_no":(123456895,12365,458963)}
+print(dict.popitem())
+print(dict)
