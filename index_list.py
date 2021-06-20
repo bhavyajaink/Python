7f2825239e771a160591b62c25f9854cee44c682
bhavyajaink <85897032+bhavyajaink@users.noreply.github.com>
2021-06-20 15:30:27 +0530

    Add files via upload
    The method index of list use in these file.

---
+++ index_list.py
@@ -0,0 +1,4 @@
+#print the index value of every element in the list
+fr=['bhvaya','komal','khushi','akshuni','divya']
+for i in fr:
+    print('Index value of element({}) is:={}'.format(i,fr.index(i)))
