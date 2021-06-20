e2c980afa13d8fbf8551a00baf65e818e104dc64
bhavyajaink <85897032+bhavyajaink@users.noreply.github.com>
2021-06-20 15:28:18 +0530

    Add files via upload
    The method copy of list use in these file.

---
+++ copy_list.py
@@ -0,0 +1,7 @@
+#we have a college in that different department are there so all the student have a common suject that is english so copy the name of all the departnment student and make a new list of that student
+# use the copy function at here.
+BCA=['bhvaya','komal','khushi','akshuni','divya']
+BCOM=['depanshi','dikshika','saloni','purvi']
+BA=['deepali','aradhana','kmala','oreo']
+english_student=BCA.copy()+BCOM.copy()+BA.copy()
+print("English student :-",english_student)
