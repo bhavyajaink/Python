226d199d5a78771847eb0be94e0c140eed34831c
bhavyajaink <85897032+bhavyajaink@users.noreply.github.com>
2021-06-20 15:32:36 +0530

    Add files via upload
    The method reverse_list of list use in these file.

---
+++ reverse_list.py
@@ -0,0 +1,5 @@
+#to reverse the element of the list
+fr=['bhvaya','komal','khushi','akshuni','divya','komal']
+print('before the reverse operation of fr value are:=',fr)
+fr.reverse()
+print('after the reverse opertion of fr value are:=',fr)
